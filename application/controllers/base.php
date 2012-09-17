5f67023ac0bf21ec0587a15ddf34d894f0f53a31
Selsie Hanson <code735@gmail.com>
2012-09-17 20:31:01 +0000

    added constructor to base controller to provide filters for certain classes

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -14,4 +14,22 @@ class Base_Controller extends Controller {
 		return Response::error('404');
 	}
 
+	public function __construct()
+    {
+     
+        parent::__construct();
+ 
+        //Filters
+        $class = get_called_class();
+        switch($class) {
+            case 'Home_Controller':
+                $this->filter('before', 'auth')->only(array('logout'));
+                $this->filter('before', 'nonauth');
+                break;
+            default:
+                $this->filter('before', 'auth');
+                break;
+        }
+    }
+
 }
