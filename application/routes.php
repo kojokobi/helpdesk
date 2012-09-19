dc8bf0b87e2f5b249d096f0cb745b06d66b83d46
Selsie Hanson <code735@gmail.com>
2012-09-19 13:27:06 +0000

    added view for tickets and navigation for dashboard

--- application/routes.php
+++ application/routes.php
@@ -81,6 +81,9 @@ Route::get("logout", array("uses" => "security@logout"));
  *Admin routes go here
  */
 Route::get("admin", array("as"=> "admin", "uses"=> "admin@index"));
+Route::get("tickets", function (){
+	return View::make("tickets.index");
+});
 Route::get('login', function()
 {
 	return View::make('login.index');
--- application/views/common/navigation.php
+++ application/views/common/navigation.php
@@ -1,6 +1,21 @@
   <div class="span2 thesidebar">
            <div class="sidebar-nav list_view" id='nav_menu'>
             <div id="navigatingAccordion" class="accordion">
+              <div class="accordion-group">
+                  <div class="accordion-heading">
+                    <a href="#dashboardCollapse" data-parent="#navigatingAccordion" data-toggle="collapse" class="accordion-toggle">
+                     <i class='icon-th-list icon'></i> Dashboard
+                    </a>
+                  </div>
+                  <div class="accordion-body collapse" id="dashboardCollapse" >
+                    <div class="accordion-inner">
+                      <ul>
+                        <li><a href="dashboard#">dashboard </a> </li>
+                        
+                      </ul>
+                    </div>
+                  </div>
+                </div> 
                 <div class="accordion-group">
                   <div class="accordion-heading">
                     <a href="#ticketsCollapse" data-parent="#navigatingAccordion" data-toggle="collapse" class="accordion-toggle">
@@ -10,11 +25,11 @@
                   <div class="accordion-body collapse" id="ticketsCollapse" >
                     <div class="accordion-inner">
                      <ul>
-                        <li><a href="#"> Unassigned tickets</a> <span class='badge pull-right'> 35 </span> </li>
-                        <li><a href="#"> Assigned tickets</a> <span class='badge badge-info pull-right'> 30 </span> </li>
-                        <li><a href="#"> Open tickets </a> <span class='badge badge-warning pull-right'> 20 </span> </li>
-                        <li><a href="#"> Closed tickets</a> <span class='badge badge-success pull-right'> 15 </span> </li>
-                        <li><a href="#"> All tickets</a> <span class='badge badge-info pull-right'> 100 </span> </li>
+                        <li><a href="tickets#unassigned"> Unassigned tickets</a> <span class='badge pull-right'> 35 </span> </li>
+                        <li><a href="tickets#assigned"> Assigned tickets</a> <span class='badge badge-info pull-right'> 30 </span> </li>
+                        <li><a href="tickets#opened"> Open tickets </a> <span class='badge badge-warning pull-right'> 20 </span> </li>
+                        <li><a href="tickets#closed"> Closed tickets</a> <span class='badge badge-success pull-right'> 15 </span> </li>
+                        <li><a href="tickets#all"> All tickets</a> <span class='badge badge-info pull-right'> 100 </span> </li>
                       </ul>
                     </div>
                   </div>
@@ -28,7 +43,7 @@
                   <div class="accordion-body in" id="adminCollapse" >
                     <div class="accordion-inner">
                       <ul>
-                        <li><a href="#">Management </a> </li>
+                        <li><a href="admin#">Management </a> </li>
                         
                       </ul>
                     </div>
