1491ff315bbd570f3289312c48f730c4a544eaa5
Selsie Hanson <code735@gmail.com>
2012-09-27 11:43:13 +0000

    fixed default route from ticket_view to tickets_view

--- application/controllers/Security.php
+++ application/controllers/Security.php
@@ -15,7 +15,7 @@ class Security_Controller extends Base_Controller{
 		$isAuth = $security->login($credentials);
 
 		if($isAuth)
-			return Redirect::to("ticket_view");
+			return Redirect::to("tickets_view");
 		else
 			return Redirect::to('login')->with('login_errors',true);
 	}
